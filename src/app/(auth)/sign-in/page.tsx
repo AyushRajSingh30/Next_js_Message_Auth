0c8729bc52faf153ebbbb238558b133c6aafc8fd
Ayush Raj Singh <ayushrajsin30+github@gmail.com>
2024-05-01 14:10:32 +0530

    created login ui

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,98 @@
-import React from 'react'
+'use client';
 
-function page() {
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/components/ui/use-toast';
+import { ApiResponse } from '@/types/ApiResponse';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
+import { useParams, useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { signInSchema } from '@/schemas/signInSchema';
+import { signIn } from 'next-auth/react';
+
+export default function VerifyAccount() {
+  const router = useRouter();
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+  });
+
+  //This sign-in is doing by Next-Auth
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+ 
+      const result= await signIn("credentials",{
+        redirect:false,
+        identifier:data.identifier,
+        password:data.password,
+       });
+       console.log(result)
+
+       if(result?.error){
+        toast({
+          title:"Loging failed",
+          description:"Incorrect username and password",
+          variant:"destructive",
+        })
+       }
+
+       if(result?.url){
+        router.replace("/dashboard")
+       }
+
+
+   
+  };
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Sign-in
+          </h1>
+          <p className="mb-4">Sign-in</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <Input {...field} name="email/username" />
+                  <p className="text-muted text-gray-400 text-sm">
+                   
+                  </p>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-export default page
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} name="password" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Sign-in</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
